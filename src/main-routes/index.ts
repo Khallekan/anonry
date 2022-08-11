85bcee220ca3bccf4c0fd86a28838cab8966343d
khallekan <okeolalekanisaac@gmail.com>
2022-08-11 19:47:14 +0100

    Add bookmark routes to main-routes

--- src/main-routes/index.ts
+++ src/main-routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userRoutes from "../users/routes";
 import entriesRoutes from "../entries/routes";
 import tagsRoutes from "../tags/routes";
+import bookmarkRoutes from "../bookmarks/routes";
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.use("/users", userRoutes);
 router.use("/entries", entriesRoutes);
 
 router.use("/tags", tagsRoutes);
+
+router.use("/bookmarks", bookmarkRoutes);
 
 export default router;
