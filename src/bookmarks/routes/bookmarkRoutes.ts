164c8af96862f2fa84cd62cc8f43455e53a971e1
khallekan <okeolalekanisaac@gmail.com>
2022-08-11 19:43:39 +0100

    Add bookmark basic routes

--- src/bookmarks/routes/bookmarkRoutes.ts
+++ src/bookmarks/routes/bookmarkRoutes.ts
@@ -1,4 +1,13 @@
 import { Router } from "express";
+import verifyToken from "../../utils/verifyToken";
+import { createBookmark, getBookmarks, removeBookmark } from "../controllers";
 const router = Router();
+
+router
+  .route("/")
+  .get(verifyToken, getBookmarks)
+  .post(verifyToken, createBookmark);
+
+router.route("/:id").delete(verifyToken, removeBookmark);
 
 export default router;
