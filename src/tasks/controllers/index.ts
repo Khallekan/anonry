8c173a817758edd770764b2085e688c28509f6d5
khallekan <okeolalekanisaac@gmail.com>
2022-09-02 21:53:35 +0100

    Task controllers

---
+++ src/tasks/controllers/index.ts
@@ -0,0 +1,7 @@
+import catchController from "../../utils/catchControllerAsyncs";
+import { Request, Response, NextFunction } from "express";
+import createPageInfo from "../../utils/createPagination";
+
+const createTask = catchController(async (req: Request, res: Response, next: NextFunction) => {
+
+})
