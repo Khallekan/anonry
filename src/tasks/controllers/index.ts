201b1a6eb8f81ee8771ac3f673f189210b794966
khallekan <okeolalekanisaac@gmail.com>
2022-09-02 22:03:51 +0100

    fix build issue

--- src/tasks/controllers/index.ts
+++ src/tasks/controllers/index.ts
@@ -2,6 +2,6 @@ import catchController from "../../utils/catchControllerAsyncs";
 import { Request, Response, NextFunction } from "express";
 import createPageInfo from "../../utils/createPagination";
 
-const createTask = catchController(async (req: Request, res: Response, next: NextFunction) => {
+// const createTask = catchController(async (req: Request, res: Response, next: NextFunction) => {
 
-})
+// })
