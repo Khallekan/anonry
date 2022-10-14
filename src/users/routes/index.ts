ea9bad359f0263a5af6f083d98c9b29e599409a8
khallekan <okeolalekanisaac@gmail.com>
2022-10-14 22:08:01 +0100

    Add delete user endpoint

--- src/users/routes/index.ts
+++ src/users/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import authRouter from "../../auth/routes";
 import profileRouter from "../../profile/routes/index";
 import verifyToken from "../../utils/verifyToken";
-import { editUser } from "../controllers";
+import { deleteUser, editUser } from "../controllers";
 
 const router = Router();
 
@@ -11,5 +11,7 @@ router.patch("/", verifyToken, editUser);
 router.use("/auth", authRouter);
 
 router.use("/profile", profileRouter);
+
+router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
