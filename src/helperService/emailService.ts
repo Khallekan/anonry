bd4e8e054a8bf522d10859888d7238544ba517a8
khallekan <okeolalekanisaac@gmail.com>
2023-03-23 15:17:46 +0100

    fix: email service

--- src/helperService/emailService.ts
+++ src/helperService/emailService.ts
@@ -1,13 +1,15 @@
+// import { google } from 'googleapis';
 import nodemailer from 'nodemailer';
 import path from 'path';
 import pug from 'pug';
+
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 465,
+  service: 'hotmail',
   secure: true,
+  port: 465,
   auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
+    user: process.env.E_U,
+    pass: process.env.APP_PASS_O,
   },
 });
 const emailPath = '../../emailViews/';
@@ -34,7 +36,7 @@ export const sendOTP = async (
     const mailOptions = {
       from: `Anonry <${process.env.EMAIL_USERNAME}>`,
       subject: 'Verify your email',
-      to: email,
+      to: process.env.EMAIL_USERNAME,
       html,
     };
     await transporter.sendMail(mailOptions);
@@ -101,13 +103,10 @@ export const sendPasswordChanged = async (
 
 export const sendLoginEmail = async (
   user_name: string,
-  email: string,
+  _email: string,
   message: string
 ) => {
   console.log('sendLoginEmail');
-
-  console.log({ user_name, email, message });
-
   try {
     const html = pug.renderFile(
       path.join(__dirname, emailPath, 'loginEmail.pug'),
@@ -120,11 +119,170 @@ export const sendLoginEmail = async (
     const mailOptions = {
       from: `Anonry <${process.env.EMAIL_USERNAME}>`,
       subject: 'Login Notification',
-      to: email,
+      to: process.env.EMAIL_USERNAME,
       html,
     };
+
+    // console.log({ mailOptions });
     await transporter.sendMail(mailOptions);
+    console.log('sent');
   } catch (error) {
     console.error(error);
   }
 };
+
+// service: 'gmail',
+// host: process.env.EMAIL_HOST,
+// port: 465,
+// secure: true,
+// auth: {
+//   user: process.env.EMAIL_USERNAME,
+//   pass: process.env.EMAIL_PASSWORD,
+//   type: 'login'
+// },
+
+// host: process.env.EMAIL_HOST,
+// auth: {
+//   // user: process.env.EMAIL_USERNAME,
+//   // pass: process.env.EMAIL_PASSWORD,
+//   type: 'OAuth2',
+//   user: process.env.EMAIL_USERNAME,
+//   clientId: process.env.OAUTH_CLIENT_ID,
+//   clientSecret: process.env.OAUTH_CLIENT_SECRET,
+//   refreshToken:
+
+// },
+
+// service: 'gmail',
+// port: 465,
+// secure: true,
+// auth: {
+//   user: process.env.EMAIL_USERNAME,
+//   pass: process.env.EMAIL_PASSWORD,
+// },
+
+// host: 'smtp.ethereal.email',
+// port: 465,
+// secure: true, // true for 465, false for other ports
+// auth: {
+//   user: testAccount.user, // generated ethereal user
+//   pass: testAccount.pass, // generated ethereal password
+// },
+
+// const createTransport = async () => {
+//   return nodemailer.createTransport({
+//     // service: 'gmail',
+//     // host: process.env.EMAIL_HOST,
+//     // port: 465,
+//     // secure: true,
+//     // auth: {
+//     //   user: process.env.EMAIL_USERNAME,
+//     //   pass: process.env.EMAIL_PASSWORD,
+//     //   type: 'login'
+//     // },
+
+//     // host: process.env.EMAIL_HOST,
+//     // auth: {
+//     //   // user: process.env.EMAIL_USERNAME,
+//     //   // pass: process.env.EMAIL_PASSWORD,
+//     //   type: 'OAuth2',
+//     //   user: process.env.EMAIL_USERNAME,
+//     //   clientId: process.env.OAUTH_CLIENT_ID,
+//     //   clientSecret: process.env.OAUTH_CLIENT_SECRET,
+//     //   refreshToken:
+//     // },
+
+//     // service: 'gmail',
+//     // port: 465,
+//     // secure: true,
+//     // auth: {
+//     //   user: process.env.EMAIL_USERNAME,
+//     //   pass: process.env.EMAIL_PASSWORD,
+//     // },
+
+//     // host: 'smtp.ethereal.email',
+//     // port: 465,
+//     // secure: true, // true for 465, false for other ports
+//     // auth: {
+//     //   user: testAccount.user, // generated ethereal user
+//     //   pass: testAccount.pass, // generated ethereal password
+//     // },
+
+//     service: 'hotmail',
+//     auth: {
+//       user: process.env.E_U,
+//       pass: process.env.E_S
+//     }
+//   });
+// };
+// const OAuth2Client = new google.auth.OAuth2(
+//   process.env.OAUTH_CLIENT_ID,
+//   process.env.OAUTH_CLIENT_SECRET,
+//   process.env.OAUTH_REDIRECT_URL
+// );
+
+// OAuth2Client.setCredentials({
+//   refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+// });
+
+// google.options({ auth: OAuth2Client });
+
+// const createTransporter = async () => {
+//   console.log('here');
+//   const accessToken = OAuth2Client.getAccessToken(async (err, res) => {
+//     console.log({ err, res });
+//   });
+//   console.log('after');
+//   console.log({ accessToken });
+//   // return nodemailer.createTransport({
+//   //   service: 'gmail',
+//   //   auth: {
+//   //     type: 'OAuth2',
+//   //     user: process.env.EMAIL_USERNAME,
+//   //     clientId: process.env.OAUTH_CLIENT_ID,
+//   //     clientSecret: process.env.OAUTH_CLIENT_SECRET,
+//   //     accessToken: acc,
+//   //     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+//   //   },
+//   // });
+// };
+
+// const transporter = nodemailer.createTransport({
+//   // service: 'gmail',
+//   // host: process.env.EMAIL_HOST,
+//   // port: 465,
+//   // secure: true,
+//   // auth: {
+//   //   user: process.env.EMAIL_USERNAME,
+//   //   pass: process.env.EMAIL_PASSWORD,
+//   //   type: 'login'
+//   // },
+
+//   // host: process.env.EMAIL_HOST,
+//   // auth: {
+//   //   // user: process.env.EMAIL_USERNAME,
+//   //   // pass: process.env.EMAIL_PASSWORD,
+//   //   type: 'OAuth2',
+//   //   user: process.env.EMAIL_USERNAME,
+//   //   clientId: process.env.OAUTH_CLIENT_ID,
+//   //   clientSecret: process.env.OAUTH_CLIENT_SECRET,
+//   //   refreshToken:
+
+//   // },
+
+//   // service: 'gmail',
+//   // port: 465,
+//   // secure: true,
+//   // auth: {
+//   //   user: process.env.EMAIL_USERNAME,
+//   //   pass: process.env.EMAIL_PASSWORD,
+//   // },
+
+//   host: 'smtp.ethereal.email',
+//   port: 587,
+//   secure: false, // true for 465, false for other ports
+//   auth: {
+//     user: testAccount.user, // generated ethereal user
+//     pass: testAccount.pass, // generated ethereal password
+//   },
+// });
